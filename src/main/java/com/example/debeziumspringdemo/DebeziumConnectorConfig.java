85e60b58baa16877984006f871c54437faa416ec
aru <szadurski.arkadiusz@gmail.com>
2025-11-25 17:50:24 +0100

    init

---
+++ src/main/java/com/example/debeziumspringdemo/DebeziumConnectorConfig.java
@@ -0,0 +1,35 @@
+package com.example.debeziumspringdemo;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.springframework.context.annotation.Bean;
+
+import io.debezium.config.Configuration;
+
+@org.springframework.context.annotation.Configuration
+public class DebeziumConnectorConfig {
+  // based off https://debezium.io/documentation/reference/2.1/connectors/mongodb.html#mongodb-connector-properties
+  @Bean
+  public Configuration mongodbConnector() throws IOException {
+    File offsetStorageTempFile = File.createTempFile("offsets_", ".dat");
+
+    return Configuration.create()
+        // engine properties
+        .with("name", "sbd-mongodb")
+        .with("connector.class", "io.debezium.connector.mongodb.MongoDbConnector")
+        .with("offset.storage", "org.apache.kafka.connect.storage.FileOffsetBackingStore")
+        .with("offset.storage.file.filename", offsetStorageTempFile.getAbsolutePath())
+        .with("offset.flush.interval.ms", "60000")
+        // connector specific properties
+        .with("mongodb.connection.string", "mongodb://admin:password@localhost:27017/?replicaSet=e77b444b513b&authSource=admin")
+        .with("topic.prefix", "sbd-mongodb-connector")
+        .with("mongodb.user", "admin")
+        .with("mongodb.password", "password")
+//        .with("mongodb.ssl.enabled", "true") // default false
+//        .with("database.include.list", "source") // default empty
+        .with("snapshot.delay.ms", "100")
+        .with("errors.log.include.messages", "true")
+        .build();
+  }
+}
---
+++ src/test/java/com/example/debeziumspringdemo/DebeziumSpringDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.debeziumspringdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DebeziumSpringDemoApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
